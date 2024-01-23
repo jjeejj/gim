9e2d2ac0e11fd0303dc045c7684b47f73a500196
iyi <jwj@wenjunjiang.com>
2024-01-23 14:41:30 +0800

    fix: 更新用户基础信息排除鉴权

--- pkg/urlwhitelist/urlwhitelist.go
+++ pkg/urlwhitelist/urlwhitelist.go
@@ -1,7 +1,8 @@
 package urlwhitelist
 
 var Business = map[string]int{
-	"/pb.BusinessExt/SignIn": 0,
+	"/pb.BusinessExt/SignIn":     0,
+	"/pb.BusinessExt/UpdateUser": 0,
 }
 
 var Logic = map[string]int{
